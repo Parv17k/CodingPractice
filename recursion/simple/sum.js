2cba2daec9388ca3fa089197f33cbe49724c98a0
Parv17k <khatriparv@gmail.com>
2021-01-18 22:49:50 -0500

    sum Tail recursion added

--- recursion/simple/sum.js
+++ recursion/simple/sum.js
@@ -2,6 +2,17 @@ function sum(num) {
     if(num===0) return 0;
     return sum(num-1)+num;
 }
+function sumTail(num,sum=0){
+    // Tail recursion for sum in JavaScript/ Node.js 
+    if(num===0)
+    return sum;
+
+    sum=sum+num;
+    return sumTail(num-1,sum);
+
+}
 
 const result =sum(10);
-console.log("Result",result);
+const resultTail=sumTail(10);
+console.log("Result of sum",result);
+console.log("Result of Tail sum :",resultTail);
